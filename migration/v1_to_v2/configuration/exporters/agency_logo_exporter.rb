3188996f7947b509230ef66602ffc8c17fffa4bf
Jasper Lyons <jasper.lyons@gmail.com>
2021-07-14 16:43:52 +0100

    Re-write the agency-logo exporter

--- migration/v1_to_v2/configuration/exporters/agency_logo_exporter.rb
+++ migration/v1_to_v2/configuration/exporters/agency_logo_exporter.rb
@@ -1,79 +1,62 @@
 # frozen_string_literal: true
 
 require_relative('configuration_exporter.rb')
-require "mini_magick"
+require 'mini_magick'
+require 'erb'
 
 # Class that get agency's logo from v1.7 and generate files to be inserted on v2.x
-class AgencyLogoExporter < ConfigurationExporter
+class AgencyLogoExporter
   def initialize(export_dir: 'seed-files', batch_size: 500)
     @export_dir = "#{export_dir}/agency_logos"
     @batch_size = batch_size
-    super(export_dir: @export_dir)
+    @template = ERB.new(DATA)
+  end
+
+  def export
+    puts 'Exporting Agencies'
+    Agency.each_slice(@batch_size).with_index do |agencies, index|
+      agencies.each do |agency|
+        next if agency['logo_key'].blank? || agency['_attachments'].blank?
+
+        puts "- Skipped logo for #{agency['_id']}. Content-type is not image/png" unless logo_is_png?(agency)
+        next unless logo_is_png?(agency)
+
+        puts "- Exporting #{agency['_id']} logo"
+        File.open("#{@export_dir}/#{logo_name(agency)}", 'wb') do |f|
+          f.write(agency.fetch_attachment(agency['logo_key']))
+        end
+
+        File.open("#{@export_dir}/agencies.#{index}.rb", 'a') do |agency_file|
+          agency_logo_loader_string = @template.result_with_hash({
+            agency_id: agency['_id'],
+            logo_name: logo_name(agency),
+            logo_key: agency['logo_key']
+          })
+
+          agency_file.write(agency_logo_loader_string)
+        end
+      end
+    end
   end
 
   private
 
-  def config_object_names
-    %w[Agency]
+  def logo_is_png?(agency)
+    MiniMagick::Image.read(agency.fetch_attachment(agency['logo_key'])).try(:mime_type) == 'image/png'
   end
 
-  def config_objects(config_name)
-    Object.const_get(config_name)
-  end
-
-  def export_config_objects(config_name, config_class)
-    export_data_objects(config_name, config_class)
-  end
-
-  def export_data_objects(object_name, config_class)
-    config_class.each_slice(@batch_size) do |objects|
-      export_object_batch(object_name, objects, @indent)
-      _i
-    end
-  end
-
-  def export_object_batch(object_name, objects, index)
-    objects.each do |object|
-      next if object['logo_key'].blank? || object['_attachments'].blank?
-
-      puts "- Skipped logo for #{object['_id']}. Content-type is not image/png" unless logo_is_png?(object)
-      next unless logo_is_png?(object)
-
-      create_file_to_export(object_name, index)
-      export_object(object_name, object)
-    end
-  end
-
-  def create_file_to_export(object_name, index)
-    script_name = "#{object_name.downcase.pluralize}.#{index}.rb"
-    @output_file = File.open("#{@export_dir}/#{script_name}", 'a')
-  end
-
-  def export_object(_object_name, object)
-    puts "- Exporting #{object['_id']} logo"
-    build_logo_file(object)
-    write_export_file(object)
-  end
-
-  def logo_is_png?(object)
-    MiniMagick::Image.read(object.fetch_attachment(object['logo_key'])).try(:mime_type) == 'image/png'
-  end
-
-  def build_logo_file(object)
-    logo_name = "#{object['_id']}-#{object['logo_key']}"
-    File.open("#{@export_dir}/#{logo_name}", 'wb') do |f|
-      f.write(object.fetch_attachment(object['logo_key']))
-    end
-  end
-
-  def write_export_file(object)
-    logo_name = "#{object['_id']}-#{object['logo_key']}"
-    @output_file.puts "\nagency = Agency.find_by(unique_id: '#{object['_id']}')"
-    @output_file.puts "logo_full = { io: File.open(\"\#{File.dirname(__FILE__)}/#{logo_name}\"), filename: '#{object['logo_key']}' }"
-    @output_file.puts "logo_icon = { io: File.open(\"\#{File.dirname(__FILE__)}/#{logo_name}\"), filename: '#{object['logo_key']}' }"
-    @output_file.puts "puts 'Adding logo to #{object['_id']}'"
-    @output_file.puts 'agency.logo_full.attach(logo_full)'
-    @output_file.puts 'agency.logo_icon.attach(logo_icon)'
-    @output_file.puts 'agency.save!'
+  def logo_name(agency)
+    "#{agency['_id']}-#{agency['logo_key'].gsub(/%s/, '-')}"
   end
 end
+__END__
+
+agency = Agency.find_by(unique_id: '<%= agency_id %>')
+puts 'Adding logo to <%= agency_id %>'
+logo_hash = {
+  io: File.open("<%= File.join(File.dirname(__FILE__), logo_name) %>"),
+  filename: '<%= logo_key %>'
+}
+agency.logo_full.attach(logo_hash)
+agency.logo_icon.attach(logo_hash)
+agency.save!
