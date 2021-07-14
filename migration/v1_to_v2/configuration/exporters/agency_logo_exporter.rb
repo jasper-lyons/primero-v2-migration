bee91af19992c036b79f97addeedbefed2301402
Jasper Lyons <jasper.lyons@gmail.com>
2021-07-14 16:56:25 +0100

    Support ruby 2.4

--- migration/v1_to_v2/configuration/exporters/agency_logo_exporter.rb
+++ migration/v1_to_v2/configuration/exporters/agency_logo_exporter.rb
@@ -3,18 +3,39 @@
 require_relative('configuration_exporter.rb')
 require 'mini_magick'
 require 'erb'
+require 'ostruct'
+require 'fileutils'
+
+class Template < OpenStruct
+  def render(template)
+    ERB.new(template).result(binding)
+  end
+end
+
+AGENCY_LOGO_TEMPLATE = <<~ERB
+
+  agency = Agency.find_by(unique_id: '<%= agency_id %>')
+  puts 'Adding logo to <%= agency_id %>'
+  logo_hash = {
+    io: File.open("<%= File.join(File.dirname(__FILE__), logo_name) %>"),
+    filename: '<%= logo_key %>'
+  }
+  agency.logo_full.attach(logo_hash)
+  agency.logo_icon.attach(logo_hash)
+  agency.save!
+ERB
 
 # Class that get agency's logo from v1.7 and generate files to be inserted on v2.x
 class AgencyLogoExporter
   def initialize(export_dir: 'seed-files', batch_size: 500)
     @export_dir = "#{export_dir}/agency_logos"
     @batch_size = batch_size
-    @template = ERB.new(DATA)
+    FileUtils.mkdir_p(@export_dir)
   end
 
   def export
     puts 'Exporting Agencies'
-    Agency.each_slice(@batch_size).with_index do |agencies, index|
+    Agency.all.each_slice(@batch_size).with_index do |agencies, index|
       agencies.each do |agency|
         next if agency['logo_key'].blank? || agency['_attachments'].blank?
 
@@ -27,11 +48,11 @@ class AgencyLogoExporter
         end
 
         File.open("#{@export_dir}/agencies.#{index}.rb", 'a') do |agency_file|
-          agency_logo_loader_string = @template.result_with_hash({
+          agency_logo_loader_string = Template.new({
             agency_id: agency['_id'],
             logo_name: logo_name(agency),
             logo_key: agency['logo_key']
-          })
+          }).render(AGENCY_LOGO_TEMPLATE)
 
           agency_file.write(agency_logo_loader_string)
         end
@@ -46,17 +67,6 @@ class AgencyLogoExporter
   end
 
   def logo_name(agency)
-    "#{agency['_id']}-#{agency['logo_key'].gsub(/%s/, '-')}"
+    "#{agency['_id']}-#{agency['logo_key'].gsub(/\s/, '-')}"
   end
 end
-__END__
-
-agency = Agency.find_by(unique_id: '<%= agency_id %>')
-puts 'Adding logo to <%= agency_id %>'
-logo_hash = {
-  io: File.open("<%= File.join(File.dirname(__FILE__), logo_name) %>"),
-  filename: '<%= logo_key %>'
-}
-agency.logo_full.attach(logo_hash)
-agency.logo_icon.attach(logo_hash)
-agency.save!
