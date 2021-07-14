50d92e2363bfdf9079eb587c385ab0afc96dfe07
Jasper Lyons <jasper.lyons@gmail.com>
2021-07-14 20:37:22 +0100

    A bit of code golf I guess, probably uneeded but I enjoyed it

--- migration/v1_to_v2/configuration/exporters/agency_logo_exporter.rb
+++ migration/v1_to_v2/configuration/exporters/agency_logo_exporter.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative('configuration_exporter.rb')
+require_relative('configuration_exporter')
 require 'mini_magick'
 require 'erb'
 require 'ostruct'
@@ -12,6 +12,8 @@ class Template < OpenStruct
   end
 end
 
+# Blank line at the beginning is important as we'll be writing this template
+# repeatedly to a file and we'd like a space between each block of code!
 AGENCY_LOGO_TEMPLATE = <<~ERB
 
   agency = Agency.find_by(unique_id: '<%= agency_id %>')
@@ -37,30 +39,23 @@ class AgencyLogoExporter
     puts 'Exporting Agencies'
     Agency.all.each_slice(@batch_size).with_index do |agencies, index|
       agencies.each do |agency|
-        next if agency['logo_key'].blank? || agency['_attachments'].blank?
-
-        puts "- Skipped logo for #{agency['_id']}. Content-type is not image/png" unless logo_is_png?(agency)
-        next unless logo_is_png?(agency)
+        next if no_logo?(agency)
+        next puts "- Skipped logo for #{agency['_id']}. Content-type is not image/png" unless logo_is_png?(agency)
 
         puts "- Exporting #{agency['_id']} logo"
-        File.open("#{@export_dir}/#{logo_name(agency)}", 'wb') do |f|
-          f.write(agency.fetch_attachment(agency['logo_key']))
-        end
-
-        File.open("#{@export_dir}/agencies.#{index}.rb", 'a') do |agency_file|
-          agency_logo_loader_string = Template.new({
-            agency_id: agency['_id'],
-            logo_name: logo_name(agency),
-            logo_key: agency['logo_key']
-          }).render(AGENCY_LOGO_TEMPLATE)
-
-          agency_file.write(agency_logo_loader_string)
-        end
+        # write the logo image it's self
+        File.write(export_path(logo_name(agency)), agency.fetch_attachment(agency['logo_key']), 'wb')
+        # write the loader to the batch file
+        File.write(export_path("agencies.#{index}.rb"), agency_logo_loader_string(agency), mode: 'a')
       end
     end
   end
 
   private
+
+  def no_logo?(agency)
+    agency['logo_key'].blank? || agency['_attachments'].blank?
+  end
 
   def logo_is_png?(agency)
     MiniMagick::Image.read(agency.fetch_attachment(agency['logo_key'])).try(:mime_type) == 'image/png'
@@ -68,5 +63,17 @@ class AgencyLogoExporter
 
   def logo_name(agency)
     "#{agency['_id']}-#{agency['logo_key'].gsub(/\s/, '-')}"
+  end
+
+  def export_path(path)
+    File.join(@export_dir, path)
+  end
+
+  def agency_logo_loader_string(agency)
+    Template.new({
+      agency_id: agency['_id'],
+      logo_name: logo_name(agency),
+      logo_key: agency['logo_key']
+    }).render(AGENCY_LOGO_TEMPLATE)
   end
 end
